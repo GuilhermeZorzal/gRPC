30b5c00459ade068e9ec762380938ec70e07a1fd
Guilherme Broedel Zorzal <guilhermezorzal2004@gmail.com>
2025-04-30 11:41:34 -0300

    feat: salvando

--- doc/Client.py
+++ doc/Client.py
@@ -2,9 +2,6 @@ import grpc
 import calculator_pb2
 import calculator_pb2_grpc
 
-def teste():
-    print("Hell")
-
 def run():
     channel = grpc.insecure_channel("localhost:50051")
     stub = calculator_pb2_grpc.CalculatorStub(channel)
